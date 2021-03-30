6152c8cafc82d0acff2c337249e8b1592175cda4
njtucker <njtucker@iastate.edu>
2021-03-30 15:16:30 -0500

    ADD, ADDI, AND

--- project2/disassembler.py
+++ project2/disassembler.py
@@ -1,1 +1,18 @@
-print('Hello world')
+with open('file.machine') as f:
+    for line in f:
+        if line.startswith('10001011000'):
+            Rm = int(line.split(' ')[1][11:15], 2)
+            Rn = int(line.split(' ')[1][22:26], 2)
+            Rd = int(line.split(' ')[1][27:31], 2)
+            print(f'ADD X{Rd}, X{Rn}, X{Rm}')
+        elif line.startswith('1001000100'):
+            Rn = int(line.split(' ')[1][22:26], 2)
+            Rd = int(line.split(' ')[1][27:31], 2)
+            Imm = int(line.split(' ')[1][11:23], 2)
+            print(f'ADDI X{Rd}, X{Rn}, #{Imm}')
+        elif line.startswith('10001010000'):
+            Rm = int(line.split(' ')[1][11:15], 2)
+            Rn = int(line.split(' ')[1][22:26], 2)
+            Rd = int(line.split(' ')[1][27:31], 2)
+            print(f'AND X{Rd}, X{Rn}, X{Rm}')
+
