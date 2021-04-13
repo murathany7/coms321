5d1518d6ab186386eb3a778282620e0310eeeb07
njtucker <njtucker@iastate.edu>
2021-04-13 00:22:00 -0500

    more instructions, LDUR STUR field fix

--- project2/disassembler.py
+++ project2/disassembler.py
@@ -1,6 +1,9 @@
 file = open('file.machine', 'rb')
 found_bytes = file.read(4)
-while(found_bytes):
+line_count = 1
+branch_targets = []
+procedure_name = f'procedure{line_count}'
+while found_bytes:
     i = int.from_bytes(found_bytes, byteorder='big')
     instruction = '{:032b}'.format(i)
     if instruction.startswith('10001011000'):
@@ -30,7 +33,9 @@ while(found_bytes):
     elif instruction.startswith('100101'):
         br_addr = int(instruction[6:32], 2)
         print(f'B {br_addr}')
-    # TODO BR
+    elif instruction.startswith('11010110000'):
+        Rn = int(instruction[22:27], 2)
+        print(f'BR {Rn}')
     elif instruction.startswith('10110101'):
         br_addr = int(instruction[8:28])
         Rt = int(instruction[27:32], 2)
@@ -53,7 +58,7 @@ while(found_bytes):
         Rn = int(instruction[22:27], 2)
         Rt = int(instruction[27:32], 2)
         dt_addr = int(instruction[11:20])
-        print(f'LDUR X{Rn}, [X{Rd}, #{dt_addr}]')
+        print(f'LDUR X{Rn}, [X{Rt}, #{dt_addr}]')
     # TODO LSL
     # TODO LSR
     elif instruction.startswith('10101010000'):
@@ -70,7 +75,7 @@ while(found_bytes):
         Rn = int(instruction[22:27], 2)
         Rt = int(instruction[27:32], 2)
         dt_addr = int(instruction[11:20])
-        print(f'STUR X{Rn}, [X{Rd}, #{dt_addr}]')
+        print(f'STUR X{Rn}, [X{Rt}, #{dt_addr}]')
     elif instruction.startswith('11001011000'):
         Rm = int(instruction[11:16], 2)
         Rn = int(instruction[22:27], 2)
@@ -81,13 +86,29 @@ while(found_bytes):
         Rd = int(instruction[27:32], 2)
         Imm = int(instruction[10:22], 2)
         print(f'SUBI X{Rd}, X{Rn}, #{Imm}')
-    # TODO SUBIS
+    elif instruction.startswith('1111000100'):
+        Rn = int(instruction[22:27], 2)
+        Rd = int(instruction[27:32], 2)
+        Imm = int(instruction[10:22], 2)
+        print(f'SUBIS X{Rd}, X{Rn}, #{Imm}')
     elif instruction.startswith('10011011000'):
         Rm = int(instruction[11:16], 2)
         Rn = int(instruction[22:27], 2)
         Rd = int(instruction[27:32], 2)
         print(f'MUL X{Rd}, X{Rn}, X{Rm}')
+    elif instruction.startswith('11101011000'):
+        Rm = int(instruction[11:16], 2)
+        Rn = int(instruction[22:27], 2)
+        Rd = int(instruction[27:32], 2)
+        print(f'SUBS X{Rd}, X{Rn}, X{Rm}')
+    elif instruction.startswith('11111111101'):
+        print('PRNT')
+    elif instruction.startswith('11111111100'):
+        print('PRNL')
+    elif instruction.startswith('11111111110'):
+        print('DUMP')
+    elif instruction.startswith('11111111111'):
+        print('HALT')
 
     found_bytes = file.read(4)
-
-
+    line_count += 1
